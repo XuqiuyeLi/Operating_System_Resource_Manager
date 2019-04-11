d6e1654d0bab30b8c61bcf3d437e0ab03b930add
SummerLi <xl1029@nyu.edu>
2019-04-11 14:25:45 -0400

    Feat
    The main program to be executed for the Resource Allocation manger

---
+++ main.java
@@ -0,0 +1,21 @@
+/* 
+ * The goal of this program is to do resource allocation using both an optimistic resource manager 
+ * and the banker's algorithm of Dijistra. 
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Resource_Allocator{
+
+	public static void main(String[] args) {
+
+	}
+
+	// read the input file of commandline
+	private static Scanner readFile(String filename){
+	 
+	}
+
+}
